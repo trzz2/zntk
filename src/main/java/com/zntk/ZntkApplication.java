7ea8182adca4ea5e8965d4989dc4b55df56e759f
zhangying <19355966293@163.com>
2026-06-02 15:35:40 +0800

    feat: 接入 MyBatis-Plus

--- src/main/java/com/zntk/ZntkApplication.java
+++ src/main/java/com/zntk/ZntkApplication.java
@@ -1,8 +1,10 @@
 package com.zntk;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.zntk.mapper")
 @SpringBootApplication
 public class ZntkApplication {
 
